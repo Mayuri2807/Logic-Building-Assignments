9c1a3472296584ab2d6b032012267852a367221b
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-06-16 19:10:44 +0530

    Add files via upload

---
+++ Assignment_No-01/Question_5.c
@@ -0,0 +1,18 @@
+/*
+2) program to print "marvellous" 5 times on screen
+*/
+
+#include<stdio.h>
+void Display()
+{
+	int i=0;
+	for(i=1;i<=5;i++)
+	{
+	  printf("MARVELLOUS\n");
+	}
+}
+int main()
+{
+	Display();
+	return 0;
+}
