a5019d604dddaa8a12934f819c30b4c605f3467c
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-06-27 23:45:21 +0530

    Create Question_01.c

---
+++ Assignment_No-07/Question_01.c
@@ -0,0 +1,29 @@
+/*
+31)write a program which accept number from user and display below pattern
+
+*/
+#include<stdio.h>
+void Display(int iNo)
+{
+
+	int iCnt=0;
+	for(iCnt=1;iCnt<=iNo;iCnt++)
+	{
+		printf("*");
+
+	}
+	for(iCnt=1;iCnt<=iNo;iCnt++)
+	{
+
+		printf("#");
+	}
+}
+int main()
+{
+
+	int iValue=0;
+	printf("Enter a number:");
+	scanf("%d",&iValue);
+	Display(iValue);
+	return 0;
+}
