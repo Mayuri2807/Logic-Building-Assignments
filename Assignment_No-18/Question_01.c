4fd9da2a8fd3a74ea14e856739204227b915490f
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-07-08 22:44:12 +0530

    Create Question_01.c

---
+++ Assignment_No-18/Question_01.c
@@ -0,0 +1,28 @@
+/*
+1)Write a program  which accept N and print first 5 multiples of N
+
+*/
+
+#include<stdio.h>
+void Display(int iNo)
+{
+
+	int iCnt=0;
+	for(iCnt=1;iCnt<=5;iCnt++)
+	{
+
+		printf("%d\n",iCnt*4);
+	}
+}
+int main()
+{
+
+	int iValue=0;
+
+	printf("Enter a number:");
+	scanf("%d",&iValue);
+
+	Display(iValue);
+
+
+}
