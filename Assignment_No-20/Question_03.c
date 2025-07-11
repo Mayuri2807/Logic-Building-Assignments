59dbe686968153c9f70f38c4deab9403c5c6f208
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-07-11 21:59:32 +0530

    Create Question_03.c

---
+++ Assignment_No-20/Question_03.c
@@ -0,0 +1,43 @@
+/*
+3)Accept number of rows and columns from user and display
+input:3 5
+output: 5 4 3 2 1
+        5 4 3 2 1
+        5 4 3 2 1
+
+*/
+#include<stdio.h>
+void Pattern(int iNo1,int iNo2)
+{
+
+       int i=0;
+       int j=0;
+       for(i=1;i<=iNo1;i++)
+       {
+              for(j=iNo2;j>=1;j--)
+              {
+                     printf("%d\t",j);
+              }
+              printf("\n");
+       }
+}
+int main()
+{
+
+       int iValue1=0;
+       int iValue2=0;
+
+       printf("Enter a first number:");
+       scanf("%d",&iValue1);
+
+        printf("Enter a second number:");
+       scanf("%d",&iValue2);
+
+       Pattern(iValue1,iValue2);
+
+       return 0;
+
+
+       
+
+}
