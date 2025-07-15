6603a3a97a9ed6a18914685ed6420ed436fd3cd5
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-07-15 22:04:04 +0530

    Create Question_05.c

---
+++ Assignment_No-23/Question_05.c
@@ -0,0 +1,49 @@
+/*
+5)Accept number of rows and columns from user and display below pattern
+input:4 4
+ouput: 1 2 3 4
+       1 * * 4
+       1 * * 4
+       1 2 3 4
+
+*/
+#include<stdio.h>
+void Pattern(int iNo1,int iNo2)
+{
+
+       int i=0;
+       int j=0;
+       for(i=1;i<=iNo1;i++)
+       {
+              for(j=1;j<=iNo2;j++)
+              {
+              	if(j==1 || j==iNo2 || i==1 || i==iNo1)
+              	{
+
+              		printf("%d\t",j);
+
+              	}
+              	else
+              	{
+              		printf("*\t");
+              	}
+              }
+              printf("\n");
+       }
+}
+int main()
+{
+
+       int iValue1=0;
+       int iValue2=0;
+
+       printf("Enter a first number:");
+       scanf("%d",&iValue1);
+
+        printf("Enter a second number:");
+       scanf("%d",&iValue2);
+
+       Pattern(iValue1,iValue2);
+
+       return 0;
+}
