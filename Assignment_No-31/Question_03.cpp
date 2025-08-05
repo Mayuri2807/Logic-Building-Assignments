b6e55b28f7f11abea870865999a798be2fbea3e5
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-08-05 12:36:05 +0530

    Create Question_03.cpp

---
+++ Assignment_No-31/Question_03.cpp
@@ -0,0 +1,35 @@
+/*3)write a program which checks 7th and 15th & 21st & 28th  bit on or off
+
+*/
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int iValue=0;
+	int iMask=0x8104040;
+	int iResult=0;
+
+	cout<<"Enter a number:\n";
+	cin>>iValue;
+
+	iResult= iValue & iMask;
+
+	if(iResult==0)
+	{
+
+		cout<<"5 & 15 & 21 & 28 bit is off\n";
+	}
+	else
+	{
+
+		cout<<"5 & 15 & 21 & 28 bit is on\n";
+	}
+ return 0;
+
+
+}
