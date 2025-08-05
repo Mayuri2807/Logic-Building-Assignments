3ca9ecf5d5fba239640e5624a90bea536961e7ad
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-08-05 12:33:31 +0530

    Create Question_01.cpp

---
+++ Assignment_No-31/Question_01.cpp
@@ -0,0 +1,34 @@
+/*1)write a program which checks whether 15 th bit on or off.
+
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int iValue=0;
+	int iMask=0x20010;
+	int iResult=0;
+
+	cout<<"Enter a number:\n";
+	cin>>iValue;
+
+	iResult= iValue & iMask;
+
+	if(iResult==0)
+	{
+
+		cout<<"15 bit is off\n";
+	}
+	else
+	{
+
+		cout<<"15 bit is on\n";
+	}
+ return 0;
+
+
+}
