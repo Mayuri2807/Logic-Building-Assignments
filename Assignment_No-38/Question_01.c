428eda6d335f8c8d3045610e71df7f015cccecaf
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-08-06 23:59:29 +0530

    Create Question_01.c

---
+++ Assignment_No-38/Question_01.c
@@ -0,0 +1,24 @@
+/*
+196)write a recursive program which display below pattern
+output: * * * * *
+*/
+#include<stdio.h>
+
+void DisplayR()
+{
+    static int  iCnt = 1;   
+
+    if( iCnt <= 5)
+    {
+        printf("*\t");
+        iCnt++;
+        DisplayR();     
+    }
+}
+
+int main()
+{
+    DisplayR();
+
+    return 0;
+}
