fcc29dcad4c24712cc74340d102522b4e564ed2b
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-08-09 22:13:31 +0530

    Create Question_03.c

---
+++ Assignment_No-40/Question_03.c
@@ -0,0 +1,35 @@
+/*3)write a recursive program which accept String from user and count number of character
+input=Hello
+output=5
+
+*/
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+int strlenX(char *str)
+{
+    int iCount = 0;
+    
+    while(*str != '\0')
+    {
+        iCount++;
+        str++;
+    }
+    return iCount;
+}
+
+int main()
+{
+    char Arr[30];
+    int iRet = 0;
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);
+
+    iRet = strlenX(Arr);
+
+    printf("String length is : %d\n",iRet);
+
+    return 0;
+}
