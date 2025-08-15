6a10353506374e0bdeeca71acf9a9be7d6301464
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-08-15 19:10:56 +0530

    Create Question_02.c

---
+++ Assignment_No-45/Question_02.c
@@ -0,0 +1,47 @@
+/*2)write a program which accept file name from user and count number of small character from
+that file.
+input:Demo.txt
+output:Number of captial chararcter are 23
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int CountSmall(char Fname[]) {
+    int fd = 0;
+    int iCount = 0;
+    char ch;
+
+    fd = open(Fname, O_RDONLY);
+    if (fd == -1) 
+    {
+        printf("Error opening file");
+        return -1;
+    }
+
+    while (read(fd, &ch, 1) == 1) {
+        if (ch >= 'a' && ch <= 'z') {
+            iCount++;
+        }
+    }
+
+    close(fd);
+    return iCount;
+}
+
+int main()
+ {
+    char FileName[30];
+    int iRet = 0;
+
+    printf("Enter file name: ");
+    scanf("%s", FileName);
+
+    iRet = CountSmall(FileName);
+    printf("Number of small characters are %d\n", iRet);
+
+    return 0;
+}
