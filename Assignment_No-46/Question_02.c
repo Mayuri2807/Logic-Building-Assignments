230a902696a215dbd8782796a0deca672a54010d
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-09-15 23:50:05 +0530

    Create Question_02.c

---
+++ Assignment_No-46/Question_02.c
@@ -0,0 +1,34 @@
+/* 2. Write application which accept file name from user and create that file.
+
+	Input : Demo.txt
+	Output : File created successfully.
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main()
+{
+    int fd;
+    char file_name[20];
+
+    printf("Enter file name : ");
+    scanf("%[^'\n']s", file_name);
+
+    fd = creat(file_name, S_IRWXU);
+
+    if (fd == -1)
+        perror("ERROR");
+    else
+        printf("File created successfully.\n");
+
+
+    close(fd);
+
+    return 0;
+}
