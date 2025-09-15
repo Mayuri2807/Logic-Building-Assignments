c14418375fcb0911260d10b16bc459666c6b164a
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2025-09-15 23:48:18 +0530

    Create Question_01.c

---
+++ Assignment_No-46/Question_01.c
@@ -0,0 +1,34 @@
+/* 1. Write application which accept file name from user and open that file in read mode.
+
+Input : Demo.txt
+Output : File opened successfully
+
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd;
+    char file_name[20];
+
+    printf("Enter file name : ");
+    scanf("%[^'\n']s", file_name);
+
+    fd = open(file_name, O_RDONLY);
+
+
+    if (fd == -1)
+        perror("ERROR");
+    else
+        printf("File opened successfully.\n");
+        
+
+    close(fd);
+
+    return 0;
+}
